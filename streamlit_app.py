c44b9b895beb067207d01c183c5a36ae1ef028be
kabips <104161529+kabips@users.noreply.github.com>
2022-04-21 14:10:25 -0400

    Update streamlit_app.py
    message with emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
-streamlit.title('Title')
-streamlit.header('Header')
-streamlit.text('text1')
-streamlit.text('text2')
-streamlit.text('text3')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
